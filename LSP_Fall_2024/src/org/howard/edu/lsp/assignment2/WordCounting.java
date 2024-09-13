d63d691f499ea3f1dd94d48934e8bfd11442a11a
mattget21 <matthew.getachew@bison.howard.edu>
2024-09-12 23:59:12 -0400

    HW2

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -1,8 +1,45 @@
 /**
-*  Name: Matthew Getachew
-*/
+ * Name: Matthew Getachew
+ */
 package org.howard.edu.lsp.assignment2;
 
-public class WordCounting {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class WordCounting {
+	public static void main(String[] args) {
+		
+		String filePath = "src/org/howard/edu/lsp/assignment2/words.text";
+				
+		try {
+			
+			List<String> lines = Files.readAllLines(Paths.get(filePath));
+			
+			Map<String, Integer> wordCountMap = new HashMap<>();
+			
+			for (String line : lines) {
+				
+				String[] words = line.toLowerCase().split("\\W+");
+					
+				for (String word : words) {
+					
+					if (word.length() > 3) {
+						
+						wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+					}
+				}
+			}
+			
+			for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+				System.out.println(entry.getKey() + "\t" + entry.getValue());
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+	}
 }
